adb06858a522c18588bd648504d9749c6fee71c7
RistoFlink <kenssi@gmail.com>
2023-10-18 16:38:35 +0300

    Implement GetEntries and DeleteEntry

--- server/routes/entries.go
+++ server/routes/entries.go
@@ -1,1 +1,69 @@
 package routes
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gin-conic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/"
+)
+
+var entryCollection *mongo.Collection = openCollection(Client, "calories")
+
+// gin gives access to ID parameters
+func AddEntry(c *gin.Context) {}
+
+func GetEntries(c *gin.Context) {
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+	var entries []bson.M
+	cursor, err := entryCollection.Find(ctx, bson.M)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+
+	if err = cursor.All(ctx, &entries); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+	defer cancel()
+	fmt.Println(entries)
+	c.JSON(http.StatusOK, entries)
+
+}
+
+func GetEntryById(c *gin.Context) {}
+
+func GetEntriesByIngredients(c *gin.Context) {}
+
+func UpdateEntry(c *gin.Context) {
+}
+
+func UpgradeIngredient(c *gin.Context) {
+
+}
+
+func DeleteEntry(c *gin.Context) {
+	entryID := c.Params.ByName("id")
+	docID, _ := primitive.ObjectIDFromHex(entryID)
+
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+	result, err := entryCollection.DeleteOne(ctx, bson.M{"_id": docID})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.h{"error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+
+	defer cancel()
+	c.JSON(http.StatusOK, result.DeletedCount)
+}
